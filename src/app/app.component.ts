59d083d61f24bb08398d4983ff1c7ee10e8096a2
Leandro Laia <misterlaia@gmail.com>
2018-11-03 05:03:54 -0300

    componentização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,7 @@ import {Component, OnInit} from "@angular/core"
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
-
-  content = 'Welcome do Meat App!'
-
   constructor() { }
-
   ngOnInit() {
   }
 
