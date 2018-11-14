575087b967b76d3d8cf7d9cd14a27595bf7c48ed
Leandro Laia <leandro.oliveira.laia@gmail.com>
2018-11-14 18:20:20 -0200

    implementacao itens pedido e forma de pagamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,9 @@ import { InputComponent } from './shared/input/input.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    {value : 'MON', label : 'Dinheiro'},
+    {value : 'DEB', label : 'Cartão Débito'},
+    {value : 'REF', label : 'Cartão Refeição'},
+    {value : 'CRE', label : 'Cartão Crédito'}
+  ]
 
   constructor() { }
 
